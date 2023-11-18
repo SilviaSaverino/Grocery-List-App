fce6d7d7c1a8532249147fd070a4443970f58555
Silvia Saverino <silviasave89@gmail.com>
2023-11-18 19:53:27 +0000

    setting up DB. Add imports and appSettings database URL

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL : "https://grocery-6651f-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
 
