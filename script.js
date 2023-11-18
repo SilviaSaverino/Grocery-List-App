b0bcf2dc9890dcaf925e88731c23f0e016b95790
Silvia Saverino <silviasave89@gmail.com>
2023-11-18 20:00:22 +0000

    initialize Firebase app and DB connection - reference MyGroceryList in the DB; add type='module' in html script to allow import/export

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL : "https://grocery-6651f-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const myGroceryListInDB = ref(database, groceryList)
 
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
